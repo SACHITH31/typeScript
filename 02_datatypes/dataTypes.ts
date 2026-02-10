247494aa1e836977bff87cc733c9ff3720505f2c
SACHITH <kasasachith@gmail.com>
2026-02-10 21:32:10 +0530

    learnt about the strings based datatype

---
+++ 02_datatypes/dataTypes.ts
@@ -0,0 +1,5 @@
+// strings
+let myName: string = 'sachith'
+let myAnotherName: string = 'ricky'
+let totalName: string = 'My name is: ' + myName + ' and another name is: '+ myAnotherName
+console.log(totalName)
