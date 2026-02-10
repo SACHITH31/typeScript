2e029286977a94c641909c3b0a1de5cc45181e5b
SACHITH <kasasachith@gmail.com>
2026-02-10 21:54:42 +0530

    learnt about union type, undefined

--- 02_datatypes/dataTypes.ts
+++ 02_datatypes/dataTypes.ts
@@ -8,9 +8,29 @@ console.log(totalName)
 // numbers
 let myAge: number = 19
 let cgpa: number = 8.7
-console.log(typeof (myAge + cgpa))
+// console.log(typeof (myAge + cgpa))
+
 
 // date
 let timeNow: Date = new Date
-console.log(timeNow.getDate() + '.' + timeNow.getMonth() + '.' + timeNow.getFullYear())
+// console.log(timeNow.getDate() + '.' + timeNow.getMonth() + '.' + timeNow.getFullYear())
 
+
+// boolean
+let trueVariable : boolean = true
+let falseVariable : boolean = false
+
+
+// UNION TYPE: In TypeScript, a union type allows a variable to hold values of multiple different types, providing flexibility while maintaining type safety. It is defined using the vertical bar (|) symbol to separate each possible type. 
+let eitherStringOrNumber1: (string | number) = 'abc'
+let eitherStringOrNumber2: (string | number) = 12
+console.log(typeof eitherStringOrNumber1)
+console.log(typeof eitherStringOrNumber2)
+
+
+// undefined
+let projectCompletionDate: (undefined | string) = undefined
+console.log(`Project completion date is: ${projectCompletionDate}`)
+const date = new Date()
+projectCompletionDate = date.getDate() + '.' +  (date.getMonth() + 1) + '.' + date.getFullYear()
+console.log(`Project completion date is: ${projectCompletionDate}`)
