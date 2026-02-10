cae8077c75f0b89c2aca4033699050a45a0c3b25
SACHITH <kasasachith@gmail.com>
2026-02-10 21:37:31 +0530

    learned about number and date

--- 02_datatypes/dataTypes.ts
+++ 02_datatypes/dataTypes.ts
@@ -3,3 +3,14 @@ let myName: string = 'sachith'
 let myAnotherName: string = 'ricky'
 let totalName: string = 'My name is: ' + myName + ' and another name is: '+ myAnotherName
 console.log(totalName)
+
+
+// numbers
+let myAge: number = 19
+let cgpa: number = 8.7
+console.log(typeof (myAge + cgpa))
+
+// date
+let timeNow: Date = new Date
+console.log(timeNow.getDate() + '.' + timeNow.getMonth() + '.' + timeNow.getFullYear())
+
